fb9b9d7a1088ea84382858f19efa335826cb0b7d
KauLuque <luquekauaneconta@gmail.com>
2024-12-03 11:32:28 -0300

    add variaveis

---
+++ Aula 02/Aula 02.py
@@ -0,0 +1,15 @@
+print('te adoro em tudo, tudo, tudo')
+print('quero mais que tudo, tudo, tudo')
+print('te amar sem limites')
+print('viver uma grande história')
+
+nome_usuario = 'Kauane Luque' # tipo -> str = string 
+idade_usuario = 19 # tipo -> int = integer
+altura_usuario = 1.64 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print ('Seja bem vinda,', nome_usuario,)
+print ('sua idade é',idade_usuario,'anos')
+print ('sua altura é de',altura_usuario,'metros')
+print ('ela é maior de idade?',maior_idade)
+
